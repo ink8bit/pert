b80333bbc02045aa8132addf772997713a7e915d
ink8bit <ink8bit@users.noreply.github.com>
2025-04-02 15:28:23 +0300

    improve error handling

--- cmd/pert/main.go
+++ cmd/pert/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/ink8bit/pert/internal/version"
@@ -32,15 +34,42 @@ func main() {
 		os.Exit(0)
 	}
 
-	if opt == 0 && real == 0 && pes == 0 {
-		fmt.Println("No value provided")
-		return
+	args := args{
+		Opt:  opt,
+		Real: real,
+		Pes:  pes,
 	}
 
-	e := pert.Expect(opt, real, pes)
-	v := pert.Variance(pes, opt)
+	res, err := run(args)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Println(output(name, e, v))
+	fmt.Println(output(name, res.Expected, res.Variance))
+}
+
+type args struct {
+	Opt  float64
+	Real float64
+	Pes  float64
+}
+
+type result struct {
+	Expected float64
+	Variance float64
+}
+
+func run(args args) (*result, error) {
+	if args.Opt == 0 && args.Real == 0 && args.Pes == 0 {
+		return nil, errors.New("No values provided")
+	}
+	e := pert.Expect(args.Opt, args.Real, args.Pes)
+	v := pert.Variance(args.Pes, args.Opt)
+	r := &result{
+		Expected: e,
+		Variance: v,
+	}
+	return r, nil
 }
 
 func output(label string, exp, dev float64) string {
